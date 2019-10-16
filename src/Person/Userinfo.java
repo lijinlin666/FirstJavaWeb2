a5bf1c83556fe32e84db414ffef7fde94b57c082
lijinlin666 <809701740@qq.com>
2019-10-16 11:40:44 +0800

    第二次提交

---
+++ src/Person/Userinfo.java
@@ -0,0 +1,22 @@
+package Person;
+
+public class Userinfo {
+    private int username;
+    private int password;
+
+    public int getUsername() {
+        return username;
+    }
+
+    public void setUsername(int username) {
+        this.username = username;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+}
